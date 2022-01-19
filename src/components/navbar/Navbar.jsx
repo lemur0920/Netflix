65b8c7cdd9de2d70a1bd2493a76c1dca71db487c
lemur0920 <lemur12@naver.com>
2022-01-19 18:28:27 +0900

    29:37

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,16 @@
 import "./Navbar.scss"
-
+import {Search, Notifications, ArrowDropDown} from '@material-ui/icons';
+import { useState } from "react";
 const Navbar = () => {
+    const[isScrolled, setIsScrolled] = useState(false);
+
+    window.onscroll = () =>{
+        setIsScrolled(window.pageYOffset === 0 ? false : true);
+        return () => (window.onscroll = null);
+    };
+    console.log(isScrolled);
     return (
-        <div className="navbar">
+        <div className={isScrolled ? "navbar scrolled" : "navbar"}>
             <div className="container">
                 <div className="left">
                     <img
@@ -16,7 +24,19 @@ const Navbar = () => {
                     <span>My List</span>
                 </div>
                 <div className="right">
-
+                    <Search className="icon"/>
+                    <span>KID</span>
+                    <Notifications className="icon"/>
+                    <img src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+                     alt=""
+                     />
+                     <div className="profile">
+                     <ArrowDropDown className="icon"/>
+                    <div className="options">
+                        <span>Settings</span>
+                        <span>Logout</span>
+                    </div>
+                     </div>
                 </div>
             </div>
         </div>
